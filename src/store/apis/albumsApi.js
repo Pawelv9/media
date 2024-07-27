7bca72af47ebd1d2310dc2559ff632d76003f303
Pavel <pawelv9@gmail.com>
2024-07-27 22:03:34 +0300

    creating an Endpoint

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,5 +1,24 @@
-import {createApi} from "@reduxjs/toolkit/query/react";
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 const albumsApi = createApi({
     reducerPath: 'albums',
+    basePath: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005'
+    }),
+    endpoints(builder) {
+        return {
+            fetchAlbums: builder.query({
+                query: () => {
+                    return {
+                        url: '/albums',
+                        params: {
+                            userId: user.id
+                        },
+                        method: 'GET'
+                    }
+                }
+            })
+        }
+    }
 })
+
