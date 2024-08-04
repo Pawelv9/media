7e00509763c34e66b4dc36303ad4c03f3895402b
Pavel <pawelv9@gmail.com>
2024-08-04 10:40:55 +0300

    Remove implementation

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -1,15 +1,23 @@
 import ExpendablePanel from "./ExpendablePanel";
 import Button from "./Button";
-
 import React from 'react';
 import ExpandablePanel from "./ExpendablePanel";
 import {GoTrashcan} from "react-icons/go";
+import {useFetchAlbumsQuery, useRemoveAlbumMutation} from "../store";
 
 function AlbumsListItem({album}) {
-    const header = <div>
-        <Button><GoTrashcan/></Button>
-        {album.title}
-    </div>;
+    const [removeAlbum, results] = useFetchAlbumsQuery();
+
+    const handleRemoveAlbum = () => {
+        removeAlbum(album);
+    }
+
+    const header = (
+        <>
+            <Button className='mr-2' loading={results.isLoading} onClick={handleRemoveAlbum}><GoTrashcan/></Button>
+            {album.title}
+        </>
+);
 
     return (
         <ExpandablePanel key={album.id} header={header}>
