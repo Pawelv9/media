fb55087894e7fa910a1a8aa890abba829a23121c
Pavel <pawelv9@gmail.com>
2024-08-04 12:38:24 +0300

    refactoring

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,7 @@
 import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
 import Skeleton from './Skeleton';
 import Button from './Button';
-import AlbumsListItem from "./AlbumsListItem";
+import AlbumsListItem from './AlbumsListItem';
 
 function AlbumsList({ user }) {
     const { data, error, isFetching } = useFetchAlbumsQuery(user);
@@ -13,23 +13,22 @@ function AlbumsList({ user }) {
 
     let content;
     if (isFetching) {
-        content = <Skeleton className='h-10 w-full' times={3} />;
+        content = <Skeleton className="h-10 w-full" times={3} />;
     } else if (error) {
         content = <div>Error loading albums.</div>;
     } else {
         content = data.map((album) => {
-return <AlbumsListItem key={album.id} album={album}/>
-
+            return <AlbumsListItem key={album.id} album={album} />;
         });
     }
 
     return (
         <div>
-            <div className='m-2 flex flex-row items-center justify-between'>
-                <h3 className='text-lg font-bold'>
-                    Albums for {user.name}
-                </h3>
-                <Button loading={results.isLoading} onClick={handleAddAlbum}>+ Add Album</Button>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+                <Button loading={results.isLoading} onClick={handleAddAlbum}>
+                    + Add Album
+                </Button>
             </div>
             <div>{content}</div>
         </div>
